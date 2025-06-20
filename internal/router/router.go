cffb3dc1d3a4beaf103b3235178a1c8a6d84e7a2
GuruOrGoru <siddharthadhakall3722@gmail.com>
2025-06-20 10:42:19 +0000

    Added cors

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,13 +3,21 @@ package router
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/guruorgoru/ushort/internal/handler"
 )
 
 func NewRouter() chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger, middleware.Recoverer)
-
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 	r.Route("/api/v1", getRoutes)
 	return r
 }
